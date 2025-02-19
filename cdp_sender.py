fd7016963b7c44a3f811cc88141453d3471bbfd7
cappern <christoffer@cappelen.org>
2025-02-19 22:16:16 +0100

    Update cdp_sender.py

--- cdp_sender.py
+++ cdp_sender.py
@@ -1,22 +1,39 @@
-#!/usr/bin/env python3
-import time
 from scapy.all import *
-from scapy.contrib.cdp import CDP, CDPTLV  # Ensure the CDP module is loaded
+from scapy.contrib.cdp import CDP, CDPMsgDeviceID, CDPMsgSoftwareVersion
+from scapy.contrib.cdp import CDPMsgPlatform, CDPMsgPortID
 
-def send_cdp():
-    iface = "eth0"  # Change this to match your network interface
-
-    cdp_packet = (
-        Ether(dst="01:00:0c:cc:cc:cc") /  # CDP uses this multicast address
-        CDP(version=2, ttl=180) /  # CDP Header
-        CDPTLV(type=1, value="Kali-Linux") /  # Device ID TLV
-        CDPTLV(type=5, value="Kali 2024 Scapy Test")  # Software Version TLV
+def send_cdp_packet(interface):
+    # Create Ethernet header (CDP uses multicast address)
+    eth = Ether(dst='01:00:0c:cc:cc:cc', type=0x011e)
+    
+    # Create LLC header
+    llc = LLC(dsap=0xaa, ssap=0xaa, ctrl=3)
+    snap = SNAP(OUI=0x00000c, code=0x2000)
+    
+    # Create CDP packet with device information
+    cdp = CDP(
+        vers=2,
+        ttl=180,
+        msg=[
+            CDPMsgDeviceID(val='Python_AP_01'),
+            CDPMsgSoftwareVersion(val='AP Software 8.5.1'),
+            CDPMsgPlatform(val='Cisco Aironet 2800'),
+            CDPMsgPortID(val=interface)
+        ]
     )
+    
+    # Combine all layers
+    packet = eth/llc/snap/cdp
+    
+    # Send the packet
+    try:
+        sendp(packet, iface=interface)
+        print(f"CDP packet sent on interface {interface}")
+    except Exception as e:
+        print(f"Error sending packet: {e}")
 
-    while True:
-        sendp(cdp_packet, iface=iface, verbose=True)
-        print("CDP packet sent on", iface)
-        time.sleep(60)  # Send packet every 60 seconds
-
+# Example usage
 if __name__ == "__main__":
-    send_cdp()
+    # Replace with your network interface
+    interface = "eth0"
+    send_cdp_packet(interface)
