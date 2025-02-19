14dc53214242fafd405f82815c3a9ebc6a6c2401
cappern <christoffer@cappelen.org>
2025-02-19 22:04:20 +0100

    Update cdp_sender.py

--- cdp_sender.py
+++ cdp_sender.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import time
 from scapy.all import *
-from scapy.layers.l2 import CDPMsgDeviceID, CDPMsgSoftwareVersion
+from scapy.contrib.cdp import CDP, CDPTLV  # Ensure the CDP module is loaded
 
 def send_cdp():
-    iface = "eth0"  # Endre hvis nødvendig
-    while True:
-        cdp_pkt = Ether(dst="01:00:0c:cc:cc:cc") / \
-                  CDPMsgDeviceID(val="Kali-Linux") / \
-                  CDPMsgSoftwareVersion(val="Kali 2024")
+    iface = "eth0"  # Change this to match your network interface
 
-        sendp(cdp_pkt, iface=iface, verbose=False)
-        print("CDP packet sent")
-        time.sleep(60)  # Vent 60 sekunder
+    cdp_packet = (
+        Ether(dst="01:00:0c:cc:cc:cc") /  # CDP uses this multicast address
+        CDP(version=2, ttl=180) /  # CDP Header
+        CDPTLV(type=1, value="Kali-Linux") /  # Device ID TLV
+        CDPTLV(type=5, value="Kali 2024 Scapy Test")  # Software Version TLV
+    )
+
+    while True:
+        sendp(cdp_packet, iface=iface, verbose=True)
+        print("CDP packet sent on", iface)
+        time.sleep(60)  # Send packet every 60 seconds
 
 if __name__ == "__main__":
     send_cdp()
